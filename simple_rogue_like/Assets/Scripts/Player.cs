8cb936cabf9c783ed1b7f85d2f6f3ad2e1eda2ad
haruSpringWinter <harukiti317@gmail.com>
2018-03-07 00:34:59 +0900

    implement moving logic for player and enemy

---
+++ simple_rogue_like/Assets/Scripts/Player.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MovingObject {
+
+    public int wallDamage = 1;
+    public int pointsPerFood = 10;
+    public int pointsPerSoda = 20;
+    public float restartLevelDelay = 1f;
+
+    private Animator animator;
+    private int food;
+
+
+	// Use this for initialization
+    protected override void Start ()
+    {
+
+        // アニメータを取得
+        animator = GetComponent<Animator>();
+
+        // GameManagerから食事で得られるポイントを取得
+        food = GameManager.instance.playerFoodPoints;
+
+        base.Start();
+	}
+
+    private void CheckIfGameOver()
+    {
+        // foodが0になればゲームオーバー
+        if (food <= 0)
+            GameManager.instance.GameOver();
+    }
+
+    protected override void AttemptMove<T>(int xDir, int yDir)
+    {
+        food--;
+
+        base.AttemptMove<T>(xDir, yDir);
+
+        RaycastHit2D hit;
+
+        CheckIfGameOver();
+
+        GameManager.instance.playersTurn = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (!GameManager.instance.playersTurn) return;
+
+        int horizontal = 0;
+        int vertical = 0;
+
+        horizontal = (int)Input.GetAxisRaw("Horizontal");
+        vertical = (int)Input.GetAxisRaw("Vertical");
+
+        // ななめ移動を禁止
+        if (horizontal != 0)
+            vertical = 0;
+
+        if (horizontal != 0 || vertical != 0)
+            AttemptMove<Wall>(horizontal, vertical);
+
+	}
+
+    // 特定のタグを持つオブジェクトと接触したときの判定
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Exit")
+        {
+            Invoke("Restart", restartLevelDelay);
+            enabled = false;
+        }
+        else if (other.tag == "Food")
+        {
+            food += pointsPerFood;
+            other.gameObject.SetActive(false);
+        }
+        else if (other.tag == "Soda")
+        {
+            food += pointsPerSoda;
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    protected override void OnCantMove<T>(T component)
+    {
+        Wall hitWall = component as Wall;
+        hitWall.DamageWall(wallDamage);
+        animator.SetTrigger("PlayerChop");
+    }
+
+    private void Restart()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+    
+    public void LoseFood(int loss)
+    {
+        // "playerHit"のトリガーを発火させる
+        animator.SetTrigger("playerHit");
+        food -= loss;
+        CheckIfGameOver();
+    }
+}
